b4b09a877b3cb76e03b991a409f9442dd986b2d9
henriqueheemann <henrique_heemann@estudante.sc.senai.br>
2021-03-18 23:56:33 -0300

    Commit atualizando - Jogo da Velha

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-$("#enviar").click(function(){
-    $.ajax({
-        url : "index.html",
-        type : 'post',
-        data : {
-             nome : "Maria Fernanda",
-             salario :'3500'
-        },
-        beforeSend : function(){
-             $("#resultado").html("ENVIANDO...");
-        }
-   })
-   .done(function(msg){
-        $("#resultado").html(msg);
-   })
-   .fail(function(jqXHR, textStatus, msg){
-        alert(msg);
-   });
-});
+$(function() {
+     var jogador = 1;
+     var jogadorVencedor = "";
+ 
+     function colunasIguais(a, b, c) {
+         var colunaA = $("#coluna" + a);
+         var colunaB = $("#coluna" + b);
+         var colunaC = $("#coluna" + c);
+ 
+         var backgroundA = $("#coluna" + a).css("background-image");
+         var backgroundB = $("#coluna" + b).css("background-image");
+         var backgroundC = $("#coluna" + c).css("background-image");
+ 
+         if ((backgroundA == backgroundB) && (backgroundB == backgroundC) && (backgroundA != "none" && backgroundA != "")) {
+             if(backgroundA.indexOf("1.png") >= 0) {
+                 jogadorVencedor = $("#jogador1").val();
+             } else {
+                 jogadorVencedor = $("#jogador2").val();
+             }
+             return true;
+         } else {
+             return false;
+         }
+     }
+ 
+     function verificarFimDeJogo() {
+         if(colunasIguais(1, 2, 3) || colunasIguais(4, 5, 6) || colunasIguais(7, 8, 9) ||
+         colunasIguais(1, 4, 7) || colunasIguais(2, 5, 8) || colunasIguais(3, 6, 9) ||
+         colunasIguais(1, 5, 9) || colunasIguais(3, 5, 7)) {
+             $("#resultado").html("<h2>O jogador " + jogadorVencedor + " ganhou!</h2>");
+             $(".coluna").off("click");
+         }
+     }
+     $(".coluna").click(function() {
+         var bg = $(this).css("background-image");
+         if(bg == "none" || bg == "") {
+             var fig = "url(" + jogador.toString() + ".png)";
+             $(this).css("background", fig);
+             jogador = (jogador == 1 ? 2 : 1);
+             verificarFimDeJogo();
+         }
+     });
+ });
+
+$("#txtJogoVelha").click(function(e){
+     e.preventDefault();
+     $("#resultadoAjax").load("jogodavelha.txt")[0]
+})
+
